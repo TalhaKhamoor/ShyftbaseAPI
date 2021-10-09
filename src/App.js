b1dd2e36db0a7f83a9b2dff4cc3387c90af00905
Talha Khamoor <talhak597@gmail.com>
2021-10-08 18:11:33 -0600

    add getRecipe function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import "./key";
+import Axios from "axios";
 
 function App() {
+  const YOUR_APP_ID = "a3528aa5";
+  const YOUR_APP_KEY = "b981ff5cc07fed4746c9e995767a238b";
+  var url = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+
+  async function getRecipes() {
+    var result = await Axios.get(url);
+    console.log(result.data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 onClick={getRecipes}>Hello world</h1>
     </div>
   );
 }
---
+++ src/key.js
@@ -0,0 +1,2 @@
+const YOUR_APP_ID = "a3528aa5";
+const YOUR_APP_KEY = "b981ff5cc07fed4746c9e995767a238b";
